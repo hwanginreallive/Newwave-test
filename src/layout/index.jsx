181d44cef284bb904ee45e24e5f0bb84a5ec1d8d
Hoang Nguyen <hwanginreallive@gmail.com>
2024-09-13 23:31:48 +0700

    feat: remove footer

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -69,13 +69,6 @@ const Wrapper = () => {
         >
           <Router />
         </Content>
-        <Footer
-          style={{
-            textAlign: "center",
-          }}
-        >
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
